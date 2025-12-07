dd706d65f404daaafe0bb10ada911e2cf6e8f07d
Firefly-0513 <kmb213d@gmail.com>
2025-12-07 16:08:04 +0800

    Changes to be committed: 	modified:   api/create-booking.js 	modified:   booking.html

---
+++ api/create-booking.js
@@ -0,0 +1,28 @@
+import { sql } from "@vercel/postgres";
+
+try {
+    const { TID, classroom,date, stime, etime, reason, people, special } = req.body;
+
+    if (!TID || !classroom || !date || !stime || !|| !etime || !reason || !people || !special) {
+      return res.status(400).json({ error: 'Please fill all the fields' });
+    }
+
+    const result = await sql`
+      INSERT INTO booking (tid, cid, bdate, stime, etime, reason, reason)
+      VALUES (${TID}, ${classroom}, ${date}, ${stime}, ${etime}, ${reason},  ${special})
+      RETURNING bid; 
+    `;
+
+res.status(200).json({ 
+    success: true, 
+    message: 'Booking successful！', 
+    booking_bid: result.rows[0].bid 
+    });
+
+  } catch (error) {
+    res.status(500).json({ 
+     error: error.message,
+     message: 'Error,Try it later！',
+    });
+  }
+} 
